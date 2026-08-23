75ae5af87bdd95afc9880f65d160dfd7753785b4
Ivan Kasyanov <ivankasyanov@MacBook-Pro-Ivan.local>
2026-08-24 01:51:29 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr()],
-	base: '/',
+	base: '/testo/',
 	resolve: {
 		alias: {
 			'@': path.resolve(import.meta.dirname, './src'),
